a9f135ffb523bd2bf61376bfe158f8782c7c4da1
Nathaniel <73431750+nyakotey@users.noreply.github.com>
2022-08-26 08:15:07 +0000

    add basic input validation allow multiple search results improve page styles:     - move info notification to the top     - adjust element spacing     - make multiple search results scroll horizontally

--- script.js
+++ script.js
@@ -2,41 +2,13 @@ const $ = (elem) => document.querySelectorAll(elem);
 const input = $("input")[0];
 const output = $(".display")[0];
 const submitButton = $("button")[0];
-const errQueryHtml = `<div class="error">Please revise your input</div>`;
 
-async function fetchDB() {
-    let resp = await fetch("countries.json");
-    json = await resp.json();
-    return json;
-}
-
-function submitOnEnter(e) {
-    if (e.keyCode == 13 || e.code == "Enter" || e.key == "Enter") {
-        response(e);
-    }
-}
 function getQuery() {
     query = input.value;
     // input.value = "";
     return query;
 }
 
-function search(db, searchArg) {
-    return db.filter((e) => e.dialCode == searchArg);
-}
-
-function res2Html(result) {
-    if (result.length == 0) {
-        return `<div class="error">Not found</div>`;
-    }
-    result = result[0];
-    return `
-<div class="country">
-    <p class="country_name">${result.name}</p>
-    <div class="country_flag"><img src="${result.flag}" alt="" class="country_flag_img"></div>
-</div>    
-`;
-}
 function validateQuery(query) {
     // valid [.+0-9\-,]
     const nonMatch = /[a-zA-Z;:' "?<>/.{}\\\[\]@_=!#$%^&*]/gm;
@@ -47,6 +19,36 @@ function validateQuery(query) {
     return true;
 }
 
+async function fetchDB() {
+    let resp = await fetch("countries.json");
+    json = await resp.json();
+    return json;
+}
+
+function search(db, searchArg) {
+    return db.filter((e) => e.dialCode == searchArg);
+}
+
+function res2Html(result) {
+    const notFoundHtml = `<div class="error">Not found</div>`;
+    let AllCountryHtml = "";
+
+    if (result.length == 0) {
+        return notFoundHtml;
+    }
+    result.forEach((c) => {
+         countryHtml = `
+<div class="country">
+<p class="country_name">${c.name}</p>
+<div class="country_flag"><img src="${c.flag}" alt="" class="country_flag_img"></div>
+</div>    
+`;
+AllCountryHtml += countryHtml; 
+    });
+   
+    return AllCountryHtml;
+}
+
 async function response() {
     query = getQuery();
     validQuery = validateQuery(query);
@@ -55,17 +57,20 @@ async function response() {
         data = search(db, query);
         output.innerHTML = res2Html(data);
     } else {
+    const errQueryHtml = `<div class="error">Please revise your input</div>`;
         output.innerHTML = errQueryHtml;
     }
 }
 
-input.addEventListener("enter", response);
-input.addEventListener("keydown", submitOnEnter);
-submitButton.addEventListener("click", response);
+function submitOnEnter(e) {
+    if (e.keyCode == 13 || e.code == "Enter" || e.key == "Enter") {
+        response(e);
+    }
+}
 
 async function tests() {
     const testRandomCountry = () => {
-        countries = ["+255", "+241", "+239", "+237", "+1"];
+        countries = ["+255", "+241", "+239", "+44", "+237", "+1", "+1264"];
         i = Math.floor(Math.random() * countries.length);
         return countries[i];
     };
@@ -74,8 +79,12 @@ async function tests() {
     // };
     // response(fake);
     db = await fetchDB();
-        data = search(db, testRandomCountry());
-        output.innerHTML = res2Html(data);
-
+    data = search(db, testRandomCountry());
+    output.innerHTML = res2Html(data);
 }
 // tests();
+
+// main
+input.addEventListener("enter", response);
+input.addEventListener("keydown", submitOnEnter);
+submitButton.addEventListener("click", response);
